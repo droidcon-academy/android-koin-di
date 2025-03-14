86b29a4f4bc5ef089849f3b2e22be10497c45424
MDeMel-Dev <mdemel.dev@gmail.com>
2025-03-15 10:20:11 +1100

    update

--- source-code/starter-project/app/src/main/java/com/droidcon/weatherscope/injection/ViewModelModule.kt
+++ source-code/starter-project/app/src/main/java/com/droidcon/weatherscope/injection/ViewModelModule.kt
@@ -7,7 +7,7 @@ import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 
 val viewModelModule = module {
-    viewModel { CurrentWeatherViewModel() }
+    viewModel { CurrentWeatherViewModel(get(), get()) }
     viewModel { ForecastViewModel() }
     viewModel { SettingsViewModel(appPreferences = get()) }
 }
