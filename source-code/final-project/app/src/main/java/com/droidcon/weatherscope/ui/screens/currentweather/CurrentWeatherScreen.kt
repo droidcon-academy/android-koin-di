7d2db222ad332307fdc94f189d565e2507cbf739
MDeMel-Dev <mdemel.dev@gmail.com>
2025-05-23 21:21:24 +1000

    cleanup design

--- source-code/final-project/app/src/main/java/com/droidcon/weatherscope/ui/screens/currentweather/CurrentWeatherScreen.kt
+++ source-code/final-project/app/src/main/java/com/droidcon/weatherscope/ui/screens/currentweather/CurrentWeatherScreen.kt
@@ -1,5 +1,6 @@
 package com.droidcon.weatherscope.ui.screens.currentweather
 
+import RotatingSettingsIconAnimation
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -33,6 +34,7 @@ import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.rotate
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
@@ -90,7 +92,7 @@ fun CurrentWeatherScreen(onNavigateToSettings: (String) -> Unit = {}) {
 
         val weatherState = (screenState as? DataState.Success)?.state
 
-        Spacer(modifier = Modifier.height(16.dp))
+        Spacer(modifier = Modifier.height(48.dp))
 
         Button(
             onClick = { viewModel.getCurrentLocationCoordinates() },
@@ -98,7 +100,7 @@ fun CurrentWeatherScreen(onNavigateToSettings: (String) -> Unit = {}) {
         ) {
             Text(stringResource(R.string.use_current_gps))
         }
-        Spacer(modifier = Modifier.height(16.dp))
+        Spacer(modifier = Modifier.height(36.dp))
 
         Row(verticalAlignment = Alignment.CenterVertically) {
             TextField(
@@ -117,17 +119,18 @@ fun CurrentWeatherScreen(onNavigateToSettings: (String) -> Unit = {}) {
             }
         }
 
-        Spacer(modifier = Modifier.height(16.dp))
+        Spacer(modifier = Modifier.height(24.dp))
 
-        Row(verticalAlignment = Alignment.CenterVertically) {
+        Row {
             Button(onClick = { onNavigateToSettings(weatherState?.data?.locationName ?: "") }) {
                 Text(stringResource(R.string.go_to_settings))
                 Spacer(modifier = Modifier.width(8.dp))
 
                 Icon(
                     imageVector = Icons.Default.Settings,
-                    contentDescription = null,
-                    tint = MaterialTheme.colorScheme.inverseOnSurface
+                    contentDescription = "Settings Icon",
+                    tint = MaterialTheme.colorScheme.inverseOnSurface,
+                    modifier = Modifier.rotate(RotatingSettingsIconAnimation())
                 )
             }
         }
